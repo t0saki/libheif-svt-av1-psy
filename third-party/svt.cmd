0e3cb88e81674e7db02f6b9bc49961abfb071699
Zhengxiao Wu <15712521+t0saki@users.noreply.github.com>
2024-11-16 00:51:54 +0800

    Update svt.cmd
    use svt-av1-psy

--- third-party/svt.cmd
+++ third-party/svt.cmd
@@ -5,9 +5,9 @@
 : # If you want to enable the SVT-AV1 encoder, please check that the WITH_SvtEnc and WITH_SvtEnc_BUILTIN CMake variables are set correctly.
 : # You will also have to set the PKG_CONFIG_PATH to "third-party/SVT-AV1/Build/linux/Release" so that the local SVT-AV1 library is found.
 
-git clone -b v2.2.1 --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
+git clone -b master --depth 1 https://github.com/gianni-rosato/svt-av1-psy.git
 
-cd SVT-AV1
+cd svt-av1-psy
 cd Build/linux
 
 ./build.sh release static no-apps prefix=$(pwd)/install install
